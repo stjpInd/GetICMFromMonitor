58ef27a11cfd63f12af5aac1a5e757f2e6e5478d
JS <jpstern@gmail.com>
2021-02-20 17:24:50 +0900

    Added documentation to the GetICMProfilePathFromWindowMonitor function

--- GetICMFromMonitor/GetICMProfilePathFromMonitorWindow.cpp
+++ GetICMFromMonitor/GetICMProfilePathFromMonitorWindow.cpp
@@ -25,6 +25,12 @@ namespace
 
 namespace ICMUtil
 {
+    /// <summary>
+    /// Gets the path of the default ICM profile for the monitor the given window is on (based on whatever MONITOR_FROM_WINDOW reports).
+    /// </summary>
+    /// <param name="hWnd">Handle to the window</param>
+    /// <param name="monitorFlags">Flags to determine which monitor to use when it can not be determined from the window, as per https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-monitorfromwindow .</param>
+    /// <returns>The path of the ICM profile, or an empty string if it could not be determined.</returns>
     std::wstring GetICMProfilePathFromWindowMonitor(HWND hWnd, DWORD monitorFlags)
     {
         std::wstring profilePath = L"";
