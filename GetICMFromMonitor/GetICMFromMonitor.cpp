fee03015af1f441811b048223d47be30b6c55c47
JS <jpstern@gmail.com>
2021-02-20 17:28:30 +0900

    Fixed formatting

--- GetICMFromMonitor/GetICMFromMonitor.cpp
+++ GetICMFromMonitor/GetICMFromMonitor.cpp
@@ -20,41 +20,41 @@ INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain
 (
-	_In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPWSTR lpCmdLine,
-	_In_ int nCmdShow
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nCmdShow
 )
 {
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
 
-	// Initialize global strings
-	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-	LoadStringW(hInstance, IDC_GETICMFROMMONITOR, szWindowClass, MAX_LOADSTRING);
-	MyRegisterClass(hInstance);
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_GETICMFROMMONITOR, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
 
-	// Perform application initialization:
-	if (!InitInstance(hInstance, nCmdShow))
-	{
-		return FALSE;
-	}
+    // Perform application initialization:
+    if (!InitInstance(hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
 
-	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_GETICMFROMMONITOR));
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_GETICMFROMMONITOR));
 
-	MSG msg;
+    MSG msg;
 
-	// Main message loop:
-	while (GetMessage(&msg, nullptr, 0, 0))
-	{
-		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-	}
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
 
-	return (int)msg.wParam;
+    return (int)msg.wParam;
 }
 
 
@@ -66,23 +66,23 @@ int APIENTRY wWinMain
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-	WNDCLASSEXW wcex{};
+    WNDCLASSEXW wcex{};
 
-	wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.cbSize = sizeof(WNDCLASSEX);
 
-	wcex.style = CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc = WndProc;
-	wcex.cbClsExtra = 0;
-	wcex.cbWndExtra = 0;
-	wcex.hInstance = hInstance;
-	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GETICMFROMMONITOR));
-	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_GETICMFROMMONITOR);
-	wcex.lpszClassName = szWindowClass;
-	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GETICMFROMMONITOR));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_GETICMFROMMONITOR);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-	return RegisterClassExW(&wcex);
+    return RegisterClassExW(&wcex);
 }
 
 //
@@ -97,46 +97,46 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-	hInst = hInstance; // Store instance handle in our global variable
+    hInst = hInstance; // Store instance handle in our global variable
 
-	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
-	if (!hWnd)
-	{
-		return FALSE;
-	}
+    if (!hWnd)
+    {
+        return FALSE;
+    }
 
-	ShowWindow(hWnd, nCmdShow);
-	UpdateWindow(hWnd);
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
 
-	return TRUE;
+    return TRUE;
 }
 
 namespace
 {
-	HWND CreateReadonlyEditControl(HWND hParent, int controlId)
-	{
-		RECT drawRect{};
-		GetClientRect(hParent, &drawRect);
-		auto hEdit = ::CreateWindowW(
-			L"EDIT",
-			NULL,
-			WS_BORDER | WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_LEFT |
-			ES_MULTILINE | ES_AUTOVSCROLL | ES_READONLY,
-			0, 0, drawRect.right, drawRect.bottom,
-			hParent,
-			(HMENU)controlId,
-			(HINSTANCE)GetWindowLongPtr(hParent, GWLP_HINSTANCE),
-			NULL);
-		::ShowWindow(hEdit, SW_SHOW);
-		return hEdit;
-	}
+    HWND CreateReadonlyEditControl(HWND hParent, int controlId)
+    {
+        RECT drawRect{};
+        GetClientRect(hParent, &drawRect);
+        auto hEdit = ::CreateWindowW(
+            L"EDIT",
+            NULL,
+            WS_BORDER | WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_LEFT |
+            ES_MULTILINE | ES_AUTOVSCROLL | ES_READONLY,
+            0, 0, drawRect.right, drawRect.bottom,
+            hParent,
+            (HMENU)controlId,
+            (HINSTANCE)GetWindowLongPtr(hParent, GWLP_HINSTANCE),
+            NULL);
+        ::ShowWindow(hEdit, SW_SHOW);
+        return hEdit;
+    }
 
-	const int ID_EDIT = 6000;
+    const int ID_EDIT = 6000;
 
-	std::wstring gProfilePath = L"";
-	HWND gHEdit = nullptr;
+    std::wstring gProfilePath = L"";
+    HWND gHEdit = nullptr;
 }
 
 //
@@ -151,83 +151,83 @@ namespace
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	switch (message)
-	{
-	case WM_COMMAND:
-	{
-		int wmId = LOWORD(wParam);
-		// Parse the menu selections:
-		switch (wmId)
-		{
-		case IDM_ABOUT:
-			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-			break;
-		case IDM_EXIT:
-			DestroyWindow(hWnd);
-			break;
-		default:
-			return DefWindowProc(hWnd, message, wParam, lParam);
-		}
-	}
-	break;
-	case WM_MOVE:
-	{
-		if (gHEdit != nullptr)
-		{
-			auto profilePath = ICMUtil::GetICMProfilePathFromWindowMonitor(hWnd);
-			if (profilePath != gProfilePath)
-			{
-				gProfilePath = profilePath;
-				::SetWindowTextW(gHEdit, gProfilePath.c_str());
-				::InvalidateRect(hWnd, nullptr, TRUE);
-			}
-		}
-		break;
-	}
-	case WM_SIZE:
-	{
-		RECT drawRect{};
-		GetClientRect(hWnd, &drawRect);
-		::SetWindowPos(gHEdit, nullptr, 0, 0, drawRect.right, drawRect.bottom, SWP_NOMOVE | SWP_NOZORDER);
-		break;
-	}
-	case WM_PAINT:
-	{
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hWnd, &ps);
-		EndPaint(hWnd, &ps);
-		break;
-	}
-	case WM_CREATE:
-		gHEdit = CreateReadonlyEditControl(hWnd, ID_EDIT);
-		gProfilePath = ICMUtil::GetICMProfilePathFromWindowMonitor(hWnd);
-		::SetWindowTextW(gHEdit, gProfilePath.c_str());
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-	return 0;
+    switch (message)
+    {
+    case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        // Parse the menu selections:
+        switch (wmId)
+        {
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+    break;
+    case WM_MOVE:
+    {
+        if (gHEdit != nullptr)
+        {
+            auto profilePath = ICMUtil::GetICMProfilePathFromWindowMonitor(hWnd);
+            if (profilePath != gProfilePath)
+            {
+                gProfilePath = profilePath;
+                ::SetWindowTextW(gHEdit, gProfilePath.c_str());
+                ::InvalidateRect(hWnd, nullptr, TRUE);
+            }
+        }
+        break;
+    }
+    case WM_SIZE:
+    {
+        RECT drawRect{};
+        GetClientRect(hWnd, &drawRect);
+        ::SetWindowPos(gHEdit, nullptr, 0, 0, drawRect.right, drawRect.bottom, SWP_NOMOVE | SWP_NOZORDER);
+        break;
+    }
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        EndPaint(hWnd, &ps);
+        break;
+    }
+    case WM_CREATE:
+        gHEdit = CreateReadonlyEditControl(hWnd, ID_EDIT);
+        gProfilePath = ICMUtil::GetICMProfilePathFromWindowMonitor(hWnd);
+        ::SetWindowTextW(gHEdit, gProfilePath.c_str());
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
 }
 
 // Message handler for about box.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	UNREFERENCED_PARAMETER(lParam);
-	switch (message)
-	{
-	case WM_INITDIALOG:
-		return (INT_PTR)TRUE;
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
 
-	case WM_COMMAND:
-		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-		{
-			EndDialog(hDlg, LOWORD(wParam));
-			return (INT_PTR)TRUE;
-		}
-		break;
-	}
-	return (INT_PTR)FALSE;
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
 }
--- GetICMFromMonitor/GetICMProfilePathFromMonitorWindow.cpp
+++ GetICMFromMonitor/GetICMProfilePathFromMonitorWindow.cpp
@@ -2,59 +2,60 @@
 
 namespace
 {
-    std::wstring GetICMProfilePathFromDC(HDC hDC)
-    {
-        // https://docs.microsoft.com/en-us/windows/win32/api/wingdi/nf-wingdi-geticmprofilew
-        // First get the buffer size
-        // If the buffer is not large enough, the function returns FALSE
-        // If not, then I guess the ICM profile is empty?
-        std::wstring profilePath = L"";
-        DWORD bufferSize = 0;
-        if (!::GetICMProfileW(hDC, &bufferSize, nullptr))
-        {
-            // Now that we have the buffer size, allocate space and get the profile name
-            std::vector<WCHAR> buffer(bufferSize, L'\0');
-            if (::GetICMProfileW(hDC, &bufferSize, buffer.data()))
-            {
-                profilePath = buffer.data();
-            }
-        }
-        return profilePath;
-    }
+	std::wstring GetICMProfilePathFromDC(HDC hDC)
+	{
+		// https://docs.microsoft.com/en-us/windows/win32/api/wingdi/nf-wingdi-geticmprofilew
+		// First get the buffer size
+		// If the buffer is not large enough, the function returns FALSE
+		// If not, then I guess the ICM profile is empty?
+		std::wstring profilePath = L"";
+		DWORD bufferSize = 0;
+		if (!::GetICMProfileW(hDC, &bufferSize, nullptr))
+		{
+			// Now that we have the buffer size, allocate space and get the profile name
+			std::vector<WCHAR> buffer(bufferSize, L'\0');
+			if (::GetICMProfileW(hDC, &bufferSize, buffer.data()))
+			{
+				profilePath = buffer.data();
+			}
+		}
+		return profilePath;
+	}
 }
 
 namespace ICMUtil
 {
-    /// <summary>
-    /// Gets the path of the default ICM profile for the monitor the given window is on (based on whatever MONITOR_FROM_WINDOW reports).
-    /// </summary>
-    /// <param name="hWnd">Handle to the window</param>
-    /// <param name="monitorFlags">Flags to determine which monitor to use when it can not be determined from the window, as per https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-monitorfromwindow .</param>
-    /// <returns>The path of the ICM profile, or an empty string if it could not be determined.</returns>
-    std::wstring GetICMProfilePathFromWindowMonitor(HWND hWnd, DWORD monitorFlags)
-    {
-        std::wstring profilePath = L"";
+	/// <summary>
+	/// Gets the path of the default ICM profile for the monitor the given window is on (based on whatever MONITOR_FROM_WINDOW reports).
+	/// </summary>
+	/// <param name="hWnd">Handle to the window</param>
+	/// <param name="monitorFlags">Flags to determine which monitor to use when it can not be determined from the window,
+	/// as per https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-monitorfromwindow .</param>
+	/// <returns>The path of the ICM profile, or an empty string if it could not be determined.</returns>
+	std::wstring GetICMProfilePathFromWindowMonitor(HWND hWnd, DWORD monitorFlags)
+	{
+		std::wstring profilePath = L"";
 
-        auto hMonitor = ::MonitorFromWindow(hWnd, monitorFlags);
-        if (hMonitor == nullptr)
-        {
-            return profilePath;
-        }
+		auto hMonitor = ::MonitorFromWindow(hWnd, monitorFlags);
+		if (hMonitor == nullptr)
+		{
+			return profilePath;
+		}
 
-        auto monitorInfo = MONITORINFOEX{};
-        monitorInfo.cbSize = sizeof(MONITORINFOEX);
-        if (!::GetMonitorInfoW(hMonitor, &monitorInfo))
-        { 
-            return profilePath; 
-        }
-        
-        auto hMonitorDC = ::CreateDCW(L"DISPLAY", monitorInfo.szDevice, nullptr, nullptr);
-        if (hMonitorDC != nullptr)
-        {
-            profilePath = GetICMProfilePathFromDC(hMonitorDC);
-            ::DeleteDC(hMonitorDC);
-        }
+		auto monitorInfo = MONITORINFOEX{};
+		monitorInfo.cbSize = sizeof(MONITORINFOEX);
+		if (!::GetMonitorInfoW(hMonitor, &monitorInfo))
+		{
+			return profilePath;
+		}
 
-        return profilePath;
-    }
+		auto hMonitorDC = ::CreateDCW(L"DISPLAY", monitorInfo.szDevice, nullptr, nullptr);
+		if (hMonitorDC != nullptr)
+		{
+			profilePath = GetICMProfilePathFromDC(hMonitorDC);
+			::DeleteDC(hMonitorDC);
+		}
+
+		return profilePath;
+	}
 }
